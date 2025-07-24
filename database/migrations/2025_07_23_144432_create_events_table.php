28c1d9edf0982d93f60c7654cf4017d2fc59f21e
erdi20 <rayadavid158@gmail.com>
2025-07-24 16:28:51 +0700

    Feat: add model dan migrasi

--- database/migrations/2025_07_23_144432_create_events_table.php
+++ database/migrations/2025_07_23_144432_create_events_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,19 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
-            $table->
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('slug')->unique();
+            $table->longText('description');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->dateTime('registration_open_date');
+            $table->dateTime('registration_close_date');
+            $table->string('location');
+            $table->decimal('price', 10, 2);
+            $table->integer('max_participants')->nullable();
+            $table->integer('current_participants')->default(0);
+            $table->enum('status', ['Draft', 'Published', 'Archived', 'Cancelled'])->default('Draft');
+            $table->string('poster_img')->nullable();
             $table->timestamps();
         });
     }
